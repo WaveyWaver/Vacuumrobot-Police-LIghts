7f3135c5580026cce412b0612fdbd2d4bcc24927
WaveyWaver <85132316+WaveyWaver@users.noreply.github.com>
2022-11-19 10:35:31 +0100

    Update Sweeps Lights.ino

--- Sweeps Lights.ino
+++ Sweeps Lights.ino
@@ -1,38 +1,100 @@
-int Led1 = 5;
-int Led2 = 6;
-int Sens = 8;
-int Read = 0;
+//Configuration of Pins
+
+int Led1 = 5;   //led pin 1
+int Led2 = 6;   //led pin 2
+int Sens = 8;   //charge detection
+
+//Setup for Random modes
+int Mode = 0;   
+int Count = 0;
+
+//Initializing Pins
 void setup() 
 {
  pinMode(Led1, OUTPUT);
  pinMode(Led2, OUTPUT);
  pinMode(Sens, INPUT);
 }
+
+//Main Code
 void loop() 
 {
-  Read = digitalRead(Sens);
-  if (Read == 1)
+  //Test if Battery is being Charged
+  if (digitalRead(Sens) == 1)
   {
-    digitalWrite(13, HIGH);
+    //Turn of LEDs 1 and 2
     digitalWrite(Led1, LOW);
     digitalWrite(Led2, LOW);
+    //Flashing Onboard LED
+    for(int i=0; i<2; i++)
+       {
+        digitalWrite(13, HIGH);
+        delay(50);
+        digitalWrite(13, LOW);
+        delay(100);
+        digitalWrite(13, HIGH);
+        delay(50);
+        digitalWrite(13, LOW);
+        delay(200);
+      }
   }
   else
   {
-    digitalWrite(13,LOW);
+    //Generate Random Numbers
+    Mode = random(1,6);
+    Count = random(5,20);
+    
+    //Starting the Lightshow
+    switch (Mode)
+    {
+      case 1:
+      classicRL(Count, 300);
+      break;
+      
+      case 2:
+      classicRL(Count, 200);
+      break;
+
+      case 3:
+      multiStrobe(Count*2,2, 100);
+      break;
+
+      case 4:
+      multiStrobe(Count,4, 50);
+      break;
+
+      case 5:
+      sweeperStrobe(Count);
+      break;
+
+      case 6:
+      inverseSweep(Count);
+      break;
+    }
   }
-  if (Read == 0)
-  {
-    for(int i=0; i<5; i++) // Classic LR
-      {
-      digitalWrite(Led1, HIGH);
-      digitalWrite(Led2, LOW);
-      delay(300);
-      digitalWrite(Led1, LOW);
-      digitalWrite(Led2, HIGH);
-      delay(300);
+}
+//Flashes the LED x times with x Speed with longer delay in between
+void multiStrobe(int n, int flashes, int speed)
+{
+  for(int i= 0; i<n; i++) //double strobe
+       {
+        delay(speed);
+        for(int i= 0; i< flashes; i++)
+        {
+          digitalWrite(Led1, HIGH);
+          digitalWrite(Led2, HIGH);
+          delay(speed/2);
+          digitalWrite(Led1, LOW);
+          digitalWrite(Led2, LOW);
+          delay(speed);
+        }
+        delay(speed);
       }
-    for(int i=0; i<10; i++) //Sweeper Style Strobe
+}
+//Multiple flashes on one side with short off times, no extra delay between LED 1 and 2
+void sweeperStrobe(int n)
+{
+      for(int i=0; i<n; i++) //Sweeper Style Strobe
     {
       digitalWrite(Led1, LOW);
       digitalWrite(Led2, LOW);
@@ -51,42 +113,42 @@ void loop()
       delay(20);
     }
  }
-    
-    
-  }
-  else
+}
+//Same as sweeperStrobe, but both LEDs are lit and the strobing one turns off
+void inverseSweep(int n)
+{
+for(int i=0; i<n; i++) //Sweeper Style Strobe
   {
-    for(int i=0; i<2; i++) //double strobe
-       {
-        digitalWrite(13, HIGH);
-        delay(50);
-        digitalWrite(13, LOW);
-        delay(100);
-        digitalWrite(13, HIGH);
-        delay(50);
-        digitalWrite(13, LOW);
-        delay(200);
-      }
+  digitalWrite(Led1, LOW);
+  digitalWrite(Led2, HIGH);
+  for(int i=0; i<4; i++)
+    {
+    digitalWrite(Led1, HIGH);
+    delay(40);
+    digitalWrite(Led1, LOW);
+    delay(30);
+    }
+    digitalWrite(Led1, HIGH);
+  for(int i=0; i<4; i++)
+    {
+    digitalWrite(Led2, HIGH);
+    delay(40);
+    digitalWrite(Led2, LOW);
+    delay(30);
+    }
+    digitalWrite(Led2, HIGH);
   }
 }
-void doubleStrobe(int i)
+//Alternating between the two LEDs
+void classicRL(int n, int speed)
 {
-  for(i; i<10; i++) //double strobe
-       {
-        digitalWrite(Led1, HIGH);
-        digitalWrite(Led2, HIGH);
-        delay(50);
-        digitalWrite(Led1, LOW);
-        digitalWrite(Led2, LOW);
-        delay(100);
-        digitalWrite(Led1, HIGH);
-        digitalWrite(Led2, HIGH);
-        delay(50);
-        digitalWrite(Led1, LOW);
-        digitalWrite(Led2, LOW);
-        delay(200);
-      }
+  for(int i=0; i<n; i++) // Classic LR
+      {
       digitalWrite(Led1, HIGH);
+      digitalWrite(Led2, LOW);
+      delay(speed);
+      digitalWrite(Led1, LOW);
       digitalWrite(Led2, HIGH);
-      delay(50);
+      delay(speed);
+      }
 }
